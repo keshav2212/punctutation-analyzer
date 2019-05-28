3bdaa8dde469a7bbaeb377182903c033212b1b92
Keshav Kumar <akeshav53@gmail.com>
2019-05-29 03:39:35 +0530

    new updates

--- texttiles/views.py
+++ texttiles/views.py
@@ -6,12 +6,14 @@ def keshav(request):
 	djtext=request.GET.get('text','default')
 	djchecki=request.GET.get('esehi','off')
 	Punctuation='''""{}[]<,>.?/!@#$%^&*()_+=|\`~;:'''
-	analyzed=""
-	for char in djtext:
-		if char not in Punctuation:
-			analyzed=analyzed+char
-	params={'purpose':'Remove Punctuation','analyze_text':analyzed}
-	return render(request,'analyize.html',params)
-	return HttpResponse("Hello Keshav <a href='/'>Back</a>")
+	if(djchecki=='on'):
+		analyzed=""
+		for char in djtext:
+			if char not in Punctuation:
+				analyzed=analyzed+char
+		params={'purpose':'Remove Punctuation','analyze_text':analyzed}
+		return render(request,'analyize.html',params)
+	else:
+		return HttpResponse("Error!")
 def chinu(request):
 	return HttpResponse("Hello Chinu")
