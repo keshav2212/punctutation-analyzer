e771d417f5220083df152c70cee21098816d86c3
Keshav Kumar <akeshav53@gmail.com>
2019-06-01 02:19:59 +0530

    some bug fixed

--- texttiles/views.py
+++ texttiles/views.py
@@ -6,6 +6,8 @@ def keshav(request):
 	djtext=request.GET.get('text','default')
 	djchecki=request.GET.get('esehi','off')
 	djchecki2=request.GET.get('UPPER','off')
+	djchecki3=request.GET.get('newlineremover','off')
+	djchecki4=request.GET.get('extraspaceremover','off')
 	Punctuation='''""{}[]<,>.?/!@#$%^&*()_+=|\`~;:'''
 	if(djchecki=='on'):
 			if(djchecki2=="on"):
@@ -31,7 +33,24 @@ def keshav(request):
 			analyzed=analyzed+char.upper()
 		params={'purpose':'For Converting in Upper case','analyze_text':analyzed}
 		return render(request,'analyize.html',params)
+	elif(djchecki3=='on'):
+		analyzed=""
+		for char in djtext:
+			if char!='\n':
+				analyzed=analyzed+char
+		params={'purpose':'New line remover','analyze_text':analyzed}
+		return render(request,'analyize.html',params)
+	elif(djchecki4=='on'):
+		analyzed=""
+		for index,char in enumerate(djtext):
+			if (djtext[index]==' ' and djtext[index+1]==' '):
+				pass
+			else:
+				analyzed=analyzed+char
+		params={'purpose':'Extra Space remover','analyze_text':analyzed}
+		return render(request,'analyize.html',params)
 	else:
-		request(HttpResponse('error404'))
+		return HttpResponse("Error404")
+
 def chinu(request):
 	return HttpResponse("Hello Chinu")
